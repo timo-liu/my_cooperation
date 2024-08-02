8100f58670fbd2f823f239e9ac3556ab66569d57
Timothy Liu <timothys.new.email@gmail.com>
2024-08-02 10:14:13 -0700

    add sweeps across num_agents

--- src/my_cooperation/Main.java
+++ src/my_cooperation/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Environment environment_true = new Environment("Python/RunFiles/server_runs_true.txt");
-		Environment environment_false = new Environment("Python/RunFiles/server_runs_false.txt");
+		Environment environment_true_60 = new Environment("Python/RunFiles/server_runs_true_60.txt");
+		Environment environment_false_60 = new Environment("Python/RunFiles/server_runs_false_60.txt");
+		
+		Environment environment_true_150 = new Environment("Python/RunFiles/server_runs_true_150.txt");
+		Environment environment_false_150 = new Environment("Python/RunFiles/server_runs_false_150.txt");
+		
+		Environment environment_true_200 = new Environment("Python/RunFiles/server_runs_true_200.txt");
+		Environment environment_false_200 = new Environment("Python/RunFiles/server_runs_false_200.txt");
 	}
 
 }
